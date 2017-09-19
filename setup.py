9da9ea7b3706aa4ab3d7f16abfb18e7c4f641a79
Matthew McConnell <bigmatt0211@ufl.edu>
2017-09-19 11:09:42 -0400

    specify versions of dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
 
+#bring in __version__ from sourcecode
+#per https://stackoverflow.com/a/17626524
+#and https://stackoverflow.com/a/2073599
+
+with open('claw/version.py') as ver:
+    exec(ver.read())
+
 setup(name='optimus',
-      version='1.1.1',
+      version=__version__,
       description='Optimus transforms and rolls out your csv data to redcap.',
       url='http://github.com/pfwhite/optimus',
       author='Patrick White',
@@ -13,5 +20,8 @@ setup(name='optimus',
               'optimus = optimus.__main__:cli_run',
           ],
       },
-      install_requires=['docopt', 'pyyaml', 'python-dateutil'],
+      install_requires=[
+          'docopt==0.6.2',
+          'pyyaml==3.12',
+          'python-dateutil==2.6.1'],
       zip_safe=False)
