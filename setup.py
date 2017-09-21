047ebdfa0832ddca8cfbea38aa16cd6160ba8686
Patrick White <pfwhite@ufl.edu>
2017-09-21 08:43:32 -0400

    Fix hcv_rna rules to properly generate the hcv_im_supplb_hcvquant

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='optimus',
-      version='1.1.1',
+      version='1.1.2',
       description='Optimus transforms and rolls out your csv data to redcap.',
       url='http://github.com/pfwhite/optimus',
       author='Patrick White',
